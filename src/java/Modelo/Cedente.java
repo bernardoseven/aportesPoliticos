42b2fa2a2f0893b8ecd3f8284c4952aba12adbb8
bernardo <bernardoseven@gmail.com>
2015-11-25 16:31:49 -0300

    comienzo funcional proyecto aportes-politicos

---
+++ src/java/Modelo/Cedente.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Bernardo
+ */
+public class Cedente {
+ public int rut;
+    public String nombre;
+    public String rubro;
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(String rubro) {
+        this.rubro = rubro;
+    }
+    
+    
+}
